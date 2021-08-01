e2d57d6c3df25afedd41f46892cba3dcd39bd931
Duke Grable <eigenfruit@gmail.com>
2021-08-01 11:55:58 -0700

    initialize / rubocop customizations

--- string2json.rb
+++ string2json.rb
@@ -1,5 +1,22 @@
+# rubocop:disable Style/FrozenStringLiteralComment
+# rubocop:disable Lint/RedundantCopDisableDirective
+# rubocop:disable Layout/TrailingEmptyLines
+# rubocop:disable Lint/RedundantCopDisableDirective
+# rubocop:disable Layout/EmptyLines
+
+
+
+# Converts some string to JSON format, mostly for usage in the DragonRuby toolkit
+class String2JSON
+  def initialize
+  end
+end
 
 
 
 
-
+# rubocop:enable Style/FrozenStringLiteralComment
+# rubocop:enable Layout/EmptyLines
+# rubocop:enable Lint/RedundantCopDisableDirective
+# rubocop:enable Layout/TrailingEmptyLines
+# rubocop:enable Lint/RedundantCopDisableDirective
