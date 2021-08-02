a8555e088d28961a0b75bccd0da5f63da9078575
Duke Grable <eigenfruit@gmail.com>
2021-08-02 05:03:07 -0700

    parse hash string to json (parse_string(hash))

--- string2json.rb
+++ string2json.rb
@@ -1,9 +1,10 @@
+# rubocop:disable Style/StringLiterals
 # rubocop:disable Style/FrozenStringLiteralComment
 # rubocop:disable Lint/RedundantCopDisableDirective
 # rubocop:disable Layout/TrailingEmptyLines
 # rubocop:disable Lint/RedundantCopDisableDirective
 # rubocop:disable Layout/EmptyLines
-
+require 'json'
 
 
 # Converts some string to JSON format, mostly for usage in the DragonRuby toolkit
@@ -23,7 +24,14 @@ class String2JSON
 
 end
 
-
+def parse_string(hash)
+  u_string = hash.to_s
+  u_string.gsub! "=>", ":"
+  u_string.gsub! '"', '\\"'
+  u_string.gsub! "nil", "null"
+  puts u_string
+end
+parse_string({ "lol" => 2, "rofl" => "hehe", "hehe" => nil })
 
 
 # rubocop:enable Style/FrozenStringLiteralComment
@@ -31,3 +39,4 @@ end
 # rubocop:enable Lint/RedundantCopDisableDirective
 # rubocop:enable Layout/TrailingEmptyLines
 # rubocop:enable Lint/RedundantCopDisableDirective
+# rubocop:enable Style/StringLiterals
