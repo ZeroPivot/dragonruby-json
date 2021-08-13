10330e170e4a5c4c7b00ce42416fb6af6a73a403
Duke Grable <eigenfruit@gmail.com>
2021-08-13 05:34:37 -0700

    require_dir.rb v0.1

--- require_dir.rb
+++ require_dir.rb
@@ -1,10 +1,11 @@
-# Directoryt Require / Require all rubyt files within a directory
+# Directory Require v0.1 / Require all rubyt files within a directory. Simple script to deal
+# with what I understand will be script number complexity in DragonRuby. This aims to reduce my init.rb file
+# to a minimum
 
 def dir_require(path)
   ruby_files = Dir.glob("#{path}/*.rb")
-  ruby_files.each do |file|
-    puts file
-    require file
+  ruby_files.each do |file|   
+    require file unless file == 'require_dir.rb'
   end
 end
 
