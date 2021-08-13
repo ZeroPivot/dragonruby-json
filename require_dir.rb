d35c2c9e8ef6fd178f18b9fa1d54beeadaff9694
Duke Grable <eigenfruit@gmail.com>
2021-08-13 05:37:44 -0700

    require_dir.rb detects its FILE_NAME

--- require_dir.rb
+++ require_dir.rb
@@ -1,12 +1,18 @@
+# rubocop:disable Style/FrozenStringLiteralComment
+
 # Directory Require v0.1 / Require all rubyt files within a directory. Simple script to deal
 # with what I understand will be script number complexity in DragonRuby. This aims to reduce my init.rb file
 # to a minimum
+FILE_NAME = __FILE__.freeze!
 
 def dir_require(path)
   ruby_files = Dir.glob("#{path}/*.rb")
   ruby_files.each do |file|   
-    require file unless file == 'require_dir.rb'
+    require file unless file == FILE_NAME
   end
 end
 
-dir_require('.')
+
+# TEST
+# dir_require('.')
+# rubocop:enable Style/FrozenStringLiteralComment
